6cc5709690df7fb02d568ff671f77efff7d8139b
Alex Redington <alex.redington@thinkrelevance.com>
2011-05-23 11:32:01 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koffeeban::Application.config.secret_token = '0c562fe2713313de78e38e20caf9fe288c1ada8ecce1d7ec7d07b940ecbf2b7600a5531f4af3dc38e3e2fdbc41342bb9554610aabd7026dc903cc01b184c959b'
